68d8b4a32897c5fbecd9cb9786fd656194991b91
unknown <rzepa8@gmail.com>
2023-01-22 19:56:23 +0000

    added to code option to save typed in events on click of a save button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timeDisplayEl = $('#currentDay');
-var inputTask = $('#input')
 var container = $('#container');
+
 
 
 function displayTime() {
@@ -17,6 +17,7 @@ setInterval(displayTime, 1000);
 
 var table = $('<table>').width('100%');
 var currentHour=moment().format('HH');
+
 for(var i=9; i<18; i++){
   
   var row = $('<tr>').addClass('row');
@@ -28,28 +29,103 @@ for(var i=9; i<18; i++){
     }
     row.append(td1);  
     if(currentHour==i){
-      var td2= $('<td>').addClass('description present').append('<input>');
+      var td2= $('<td>').addClass('description present').attr('id', 'td'+i);
+      var input=$('<input>').attr('id', 'task'+i);
       row.append(td2);
+      td2.append(input)
     }else if(currentHour>i){
-      var td2= $('<td>').addClass('description past').append('<input>');
+      var td2= $('<td>').addClass('description past').attr('id', 'td'+i);
+      var input=$('<input>').attr('id', 'task'+i);
       row.append(td2);
+      td2.append(input)
+      
     }else if(currentHour<i){
-      var td2= $('<td>').addClass('description future').append('<input>');
+      var td2= $('<td>').addClass('description future').attr('id', 'td'+i);
+      var input=$('<input>').attr('id', 'task'+i);
       row.append(td2);
+      td2.append(input)
     }
   var td3= $('<td>').addClass('saveBtn');
-  var button=$('<button>');
-  var icon=$('<i>').addClass('fa-solid fa-floppy-disk')
+  var button=$('<button>').attr('id', 'save'+i);
+  var icon=$('<i>').addClass('fa-solid fa-floppy-disk');
   row.append(td3);
   td3.append(button);
   button.append(icon);
   
 }
 container.append(table);
-//console.log(currentHour)
-inputTask.on('click', function(event){
+
+function pop_up(){
+  var begin_size = 0;
+    var end_size = 30;
+    var speed = 500;
+    var speed2 = 500;
+
+    $("#savemessage").css("opacity", begin_size).animate({"opacity" : end_size}, speed);
+    setTimeout(() => {$("#savemessage").css("opacity", end_size).animate({"opacity" : begin_size}, speed2)},1000)
+    
+}
+
+$('#task9').val(localStorage.getItem('set1'));
+$('#task10').val(localStorage.getItem('set2'));
+$('#task11').val(localStorage.getItem('set3'));
+$('#task12').val(localStorage.getItem('set4'));
+$('#task13').val(localStorage.getItem('set5'));
+$('#task14').val(localStorage.getItem('set6'));
+$('#task15').val(localStorage.getItem('set7'));
+$('#task16').val(localStorage.getItem('set8'));
+$('#task17').val(localStorage.getItem('set9'));
+     
 
 
 
-  
-})
+
+  $('#save9').on('click', function(){
+    var inputTask = $('#task9').val()
+    localStorage.setItem('set1', inputTask);
+    pop_up();
+  })
+  $('#save10').on('click', function(){
+    
+    var inputTask = $('#task10').val()
+    localStorage.setItem('set2', inputTask);
+    pop_up()
+  })
+  $('#save11').on('click', function(){
+    var inputTask = $('#task11').val()
+    localStorage.setItem('set3', inputTask);
+    pop_up()
+  })
+  $('#save12').on('click', function(){
+    var inputTask = $('#task12').val()
+    localStorage.setItem('set4', inputTask);
+    pop_up()
+  })
+  $('#save13').on('click', function(){
+    var inputTask = $('#task13').val()
+    localStorage.setItem('set5', inputTask);
+    pop_up()
+  })
+  $('#save14').on('click', function(){
+    var inputTask = $('#task14').val()
+    localStorage.setItem('set6', inputTask);
+    pop_up()
+  })
+  $('#save15').on('click', function(){
+    var inputTask = $('#task15').val()
+    localStorage.setItem('set7', inputTask);
+    pop_up()
+  })
+  $('#save16').on('click', function(){
+    var inputTask = $('#task16').val()
+    localStorage.setItem('set8', inputTask);
+    pop_up()
+  })
+  $('#save17').on('click', function(){
+    var inputTask = $('#task17').val()
+    localStorage.setItem('set9', inputTask);
+    pop_up()
+  })
+   
+   
+
